100fbb8193b4257e4ee74ee2070cc51c878cf39f
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-11 17:33:18 -0300

    agrego estilos

--- src/components/moviesCard.js
+++ src/components/moviesCard.js
@@ -1,18 +1,20 @@
-
+import styles from './moviesCard.module.css'
 export default function MoviesCard ({movie}){
     
     //para obtener la ruta a la imagen
     const imageUrl= "https://image.tmdb.org/t/p/w300" + movie.poster_path;
 
     return(
-        <div className='logo-contenedor'>
+        <li className={styles.movieCard}>
             <img
             src={imageUrl}
-            className='imagen'
+            className={styles.movieImage}
+            width={230}
+            height={345}
             alt={movie.title} />
             <div>
                 {movie.title}
             </div>
-      </div>
+      </li>
     )
 }
--- src/components/moviesGrid.js
+++ src/components/moviesGrid.js
@@ -1,10 +1,12 @@
+import styles from './moviesGrid.module.css'
 import movies from '../movies.json'
 import MoviesCard from './moviesCard';
 
+
 export default function MoviesGrid (){
-    console.log (movies);
+    
     return(
-        <div>
+        <div className={styles.movieGrid} >
 
         {movies.map((movie) => (
                          <MoviesCard  key={movie.id} movie={movie} />
