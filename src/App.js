566fd2faef8333e57a9cb11c42bfa66cfa067eaf
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-11 16:47:14 -0300

    agrego NavBar con material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import MoviesGrid from './components/moviesGrid';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       < MoviesGrid />
     </div>
   );
