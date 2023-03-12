84d49d36f6079794640aa4710722d437e2d3d37f
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-12 10:29:33 -0300

    conecto con API

--- src/components/moviesGrid.js
+++ src/components/moviesGrid.js
@@ -1,10 +1,24 @@
 import styles from './moviesGrid.module.css'
-import movies from '../movies.json'
 import MoviesCard from './moviesCard';
-
+import { useEffect, useState } from 'react';
 
 
 export default function MoviesGrid (){
+
+   const [movies, setMovies]= useState([]);
+
+    useEffect( () =>{
+        fetch("https://api.themoviedb.org/3/discover/movie", {
+            headers:{
+             Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MzdmOWRmMjZiNDZlYzM3ZTBkNzg3NTMxY2Q1NDJiMyIsInN1YiI6IjY0MGQzMWUxMzIzZWJhMDEwYjkwNjIyYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CZ4aLBP4njQNk9I3D3JzRYODyWokcwKU33fQmElf3hs",    
+                "Content-Type": "application/json;charset=utf-8",
+            },
+        }).then(result => result.json()).then(data =>{
+            setMovies(data.results);
+        })
+
+      }, []);
     
     return(
         <div>
