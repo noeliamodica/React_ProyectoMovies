130ceb771d1f3546c91fbcedf16b8cae4b93a2cd
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-12 18:13:00 -0300

    agrego componente de busqueda con icono

---
+++ src/components/search.js
@@ -0,0 +1,14 @@
+import styles from './search.module.css'
+import { FaSearch } from "react-icons/fa"
+
+export default function Search (){
+    return (
+        <form className={styles.searchContainer} >
+            <div className={styles.searchBox}>
+            <input className={styles.searchInput} type="text" />
+            <button className={styles.searchButton} type="submit"><FaSearch  size={20} /></button>
+                
+            </div>
+        </form>
+    )
+}
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,9 +1,10 @@
 import MoviesGrid from "../components/moviesGrid";
+import Search from "../components/search";
 
 export default function LandingPage(){
     return (
     <div>
-        
+        <Search />
         <MoviesGrid />
     </div>
     )
