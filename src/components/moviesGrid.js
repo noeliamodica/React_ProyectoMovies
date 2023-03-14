b64cbd4cc2d5d5cf42412025bc17ea45585273b7
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-14 09:11:46 -0300

    cambios generales

--- src/components/moviesGrid.js
+++ src/components/moviesGrid.js
@@ -2,30 +2,22 @@ import styles from './moviesGrid.module.css'
 import MoviesCard from './moviesCard';
 import { useEffect, useState } from 'react';
 import { get } from '../utils/httpClient';
-import { useLocation } from 'react-router';
-import { useSearchParams } from 'react-router-dom';
-
-
-const [query, setQuery] = useSearchParams();
 
 
 export default function MoviesGrid (){
 
    const [movies, setMovies]= useState([]);
 
-   const query = useQuery();
-   const search = query.get('search');
-   console.log('search')
-   
+ 
 
     useEffect( () =>{
-        const searchUrl = search ? "/search/moviequery" + search  : "/discover/movie";
-        get(searchUrl)
+  
+        get("/discover/movie")
         .then(data =>{
             setMovies(data.results);
         })
 
-      }, [search]);
+      }, []);
     
     return(
         <div>
